a15e14fe68dd48b6054777c616360aa397a6be30
Stefan Dragoi <>
2023-05-05 23:24:17 +0300

    disertation paper

--- ArgumentationFramework/src/main/java/Main.java
+++ ArgumentationFramework/src/main/java/Main.java
@@ -8,36 +8,47 @@ public class Main {
     public static void main(String[] args) {
         ArgumentationFramework argumentationFramework = new ArgumentationFramework();
 
-//        argumentationFramework.setArguments(new ArrayList<>(List.of("A", "B", "C", "D")));
+        /*
+            Example 1
+         */
+//        argumentationFramework.setArguments(new ArrayList<>(List.of("A", "B", "C", "D", "E", "F", "G")));
 //        argumentationFramework.addAttack("A", "B");
-//        argumentationFramework.addAttack("B", "C");
-//        argumentationFramework.addAttack("C", "D");
-//        argumentationFramework.addAttack("D", "A");
-
-//        argumentationFramework.setArguments(new ArrayList<>(List.of("A", "B", "C", "D")));
 //        argumentationFramework.addAttack("B", "A");
-//        argumentationFramework.addAttack("C", "B");
-//        argumentationFramework.addAttack("D", "B");
+//        argumentationFramework.addAttack("A", "C");
+//        argumentationFramework.addAttack("C", "D");
+//        argumentationFramework.addAttack("D", "F");
+//        argumentationFramework.addAttack("D", "E");
+//        argumentationFramework.addAttack("F", "G");
+//        argumentationFramework.addAttack("E", "G");
 
-        argumentationFramework.setArguments(new ArrayList<>(List.of("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M")));
+        /*
+            Example 2
+         */
+//        argumentationFramework.setArguments(new ArrayList<>(List.of("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M")));
+//        argumentationFramework.addAttack("A", "B");
+//        argumentationFramework.addAttack("B", "D");
+//        argumentationFramework.addAttack("D", "C");
+//        argumentationFramework.addAttack("C", "A");
+//        argumentationFramework.addAttack("D", "E");
+//        argumentationFramework.addAttack("E", "F");
+//        argumentationFramework.addAttack("G", "F");
+//        argumentationFramework.addAttack("J", "G");
+//        argumentationFramework.addAttack("D", "L");
+//        argumentationFramework.addAttack("L", "J");
+//        argumentationFramework.addAttack("H", "G");
+//        argumentationFramework.addAttack("I", "G");
+//        argumentationFramework.addAttack("H", "I");
+//        argumentationFramework.addAttack("I", "H");
+//        argumentationFramework.addAttack("K", "H");
+//        argumentationFramework.addAttack("K", "I");
+//        argumentationFramework.addAttack("M", "K");
+//        argumentationFramework.addAttack("K", "M");
+
+        argumentationFramework.setArguments(new ArrayList<>(List.of("A", "B", "C", "D")));
         argumentationFramework.addAttack("A", "B");
-        argumentationFramework.addAttack("B", "D");
-        argumentationFramework.addAttack("D", "C");
-        argumentationFramework.addAttack("C", "A");
-        argumentationFramework.addAttack("D", "E");
-        argumentationFramework.addAttack("E", "F");
-        argumentationFramework.addAttack("G", "F");
-        argumentationFramework.addAttack("J", "G");
-        argumentationFramework.addAttack("D", "L");
-        argumentationFramework.addAttack("L", "J");
-        argumentationFramework.addAttack("H", "G");
-        argumentationFramework.addAttack("I", "G");
-        argumentationFramework.addAttack("H", "I");
-        argumentationFramework.addAttack("I", "H");
-        argumentationFramework.addAttack("K", "H");
-        argumentationFramework.addAttack("K", "I");
-        argumentationFramework.addAttack("M", "K");
-        argumentationFramework.addAttack("K", "M");
+        argumentationFramework.addAttack("B", "A");
+        argumentationFramework.addAttack("A", "C");
+        argumentationFramework.addAttack("C", "D"); 
 
         Algorithm algorithm = new Algorithm(argumentationFramework);
 
